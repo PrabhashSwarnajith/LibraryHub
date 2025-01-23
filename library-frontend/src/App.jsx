8fb1ab70a6febdb4e71f332ece028fdb6f0d3b3b
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-01-23 17:13:44 +0530

    add axios dependency and implement AuthService for user authentication

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,12 +1,28 @@
-
+import React,{useState} from "react"
 
 function App() {
+
+  const [text , setText] = useState(12);
+  const change = (e) =>{
+    console.log(event.target.value);
+
+    setText(e.target.value);
+  }
+
 
   return (
     <div>
       <h1>Library</h1>
       <p>Books in your library</p>
+      
+      <div>
+      <input type="text" value={text} onChange={change} />
+      </div>
+      <div>
+      <p>{text}</p>
+      </div>
     </div>
+    
   )
 }
 
