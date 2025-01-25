d057c72ff0c4b6407e1d40f6ea923f045bd89575
N1331741 <amindu.balasooriyamudiyanselage2024@my.ntu.ac.uk>
2025-01-25 18:41:01 +0530

    refactor UserRepository to remove username-based methods and use email for user lookup

--- library-backend/src/main/java/com/libraryhub/library_backend/repository/UserRepository.java
+++ library-backend/src/main/java/com/libraryhub/library_backend/repository/UserRepository.java
@@ -7,11 +7,8 @@ import com.libraryhub.library_backend.model.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
-
-    User findByUsername(String username);
 
     User findByEmail(String email);
 
